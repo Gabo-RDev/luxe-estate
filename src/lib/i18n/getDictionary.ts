5826a143172e1e74663b9250f2e9060a72b341d1
Gabriel Duarte <gabriel.duarte18@outlook.es>
2026-03-26 10:37:09 -0400

    feat: implement social login with supabase, rsc refactor, and full i18n

--- src/lib/i18n/getDictionary.ts
+++ src/lib/i18n/getDictionary.ts
@@ -1,15 +1,12 @@
 import 'server-only';
 import { Locale } from '@/types/I18n';
 
-const dictionaries: Record<Locale, () => Promise<any>> = {
+const dictionaries = {
 	es: () => import('./dictionaries/es.json').then((module) => module.default),
 	en: () => import('./dictionaries/en.json').then((module) => module.default),
 	fr: () => import('./dictionaries/fr.json').then((module) => module.default),
 };
 
 export const getDictionary = async (locale: Locale) => {
-	if (dictionaries[locale]) {
-		return dictionaries[locale]();
-	}
-	return dictionaries.es();
+	return (dictionaries[locale] || dictionaries.es)();
 };
